16fe1cf96a76d5a70ee3dddc1508573dd91bb815
aiacuone <aiacuone@users.noreply.github.com>
2021-10-14 17:15:52 +0100

    added project contact me buttons to contact me

--- pages/contactMe.js
+++ pages/contactMe.js
@@ -2,6 +2,11 @@ import React, { useContext } from 'react'
 import { makeStyles } from '@mui/styles'
 import Grid from '@mui/material/Grid'
 import { UserContext } from '../utils/UserContext'
+import Image from 'next/image'
+import email from '../public/images/email.svg'
+import linkedin from '../public/images/linkedin.svg'
+import phone from '../public/images/phone.svg'
+import gitHub from '../public/images/gitHub.svg'
 
 const useStylesRoot = makeStyles({
   root: { height: '100%', width: '100%' },
@@ -9,6 +14,18 @@ const useStylesRoot = makeStyles({
 
 export default function contactMe() {
   const classesRoot = useStylesRoot()
+
+  const buttonsObj = {
+    phone: { name: 'Phone', image: phone },
+    linkedIn: { name: 'Linkedin', image: linkedin },
+    github: { name: 'GitHub', image: gitHub },
+
+    email: { name: 'Email', image: email },
+  }
+
+  const buttonsArr = Object.keys(buttonsObj).map((button) => {
+    return button
+  })
 
   const { state, vars } = useContext(UserContext)
   const { isPhone, isPhoneLandscape } = state.phone
@@ -43,8 +60,8 @@ export default function contactMe() {
           width: '100%',
           height: '100%',
           display: 'grid',
-          gridTemplateRows: '20px  repeat(8,1fr)',
-          gridTemplateColumns: `100px repeat(8,1fr) ${
+          gridTemplateRows: '30px  repeat(8,1fr)',
+          gridTemplateColumns: `80px repeat(8,1fr) ${
             hamburger.padding * 2 + hamburger.width
           }px`, //WIDTH OF BUTTON CONTAINER
         },
@@ -64,12 +81,35 @@ export default function contactMe() {
           gridArea: '2/10/11/11',
           background: 'lime',
         },
+        button: {
+          background: 'white',
+        },
       })
       const classes = useStyles()
+
+      const buttons = buttonsArr.map((button) => {
+        return (
+          <Image
+            className={classes.button}
+            src={buttonsObj[button].image}
+            layout="fixed"
+            width={50}
+            height={50}
+            alt=""
+          />
+        )
+      })
       return (
         <Grid className={classes.root}>
           <Grid className={classes.header}>CONTACT ME</Grid>
-          <Grid className={classes.buttonContainer}>BUTTON CONTAINER</Grid>
+          <Grid
+            justifyContent="space-around"
+            alignItems="center"
+            className={classes.buttonContainer}
+            container
+            direction="column">
+            {buttons}
+          </Grid>
           <Grid className={classes.mainContainer}>MAIN CONTAINER</Grid>
           <Grid className={classes.hamburgerGap}></Grid>
         </Grid>
@@ -82,25 +122,21 @@ export default function contactMe() {
           width: '100%',
           height: '100%',
           display: 'grid',
-          gridTemplateRows: `20px 30px repeat(17,1fr) ${
+          gridTemplateRows: `20px 30px repeat(16,1fr) 70px ${
             hamburger.padding * 2 + hamburger.width
-          }px`,
+          }px `,
           gridTemplateColumns: 'repeat(10,1fr)',
         },
         header: {
           gridArea: '1/1/2/11',
           background: 'blue',
         },
-        projectHeader: {
-          gridArea: '2/1/3/11',
-          background: 'purple',
-        },
         mainContainer: {
-          gridArea: '3/1/18/11',
+          gridArea: '2/1/19/11',
           background: 'orange',
         },
         buttonContainer: {
-          gridArea: '18/1/20/11',
+          gridArea: '19/1/20/11',
           background: 'yellow',
         },
         hamburgerGap: {
@@ -109,12 +145,30 @@ export default function contactMe() {
         },
       })
       const classes = useStyles()
+
+      const buttons = buttonsArr.map((button) => {
+        return (
+          <Image
+            className={classes.button}
+            src={buttonsObj[button].image}
+            layout="fixed"
+            width={50}
+            height={50}
+            alt=""
+          />
+        )
+      })
       return (
         <Grid className={classes.root}>
-          <Grid className={classes.header}>SKILLS</Grid>
-          <Grid className={classes.projectHeader}>SKILL HEADER</Grid>
+          <Grid className={classes.header}>CONTACT ME</Grid>
           <Grid className={classes.mainContainer}>MAIN CONTAINER</Grid>
-          <Grid className={classes.buttonContainer}>BUTTON CONTAINER</Grid>
+          <Grid
+            className={classes.buttonContainer}
+            container
+            justifyContent="space-around"
+            alignItems="center">
+            {buttons}
+          </Grid>
           <Grid className={classes.hamburgerGap} />
         </Grid>
       )
