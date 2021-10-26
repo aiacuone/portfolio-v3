666ae8a0ff407f7050ca7a5266800590cb103a80
aiacuone <aiacuone@users.noreply.github.com>
2021-10-26 11:37:15 +0100

    added cheapshark project header

--- components/CheapsharkProjectHeader.js
+++ components/CheapsharkProjectHeader.js
@@ -3,33 +3,82 @@ import Grid from '@mui/material/Grid'
 import { makeStyles } from '@mui/styles'
 import Image from 'next/image'
 import { UserContext } from '../utils/UserContext'
+import logo from '../public/images/projectHeaders/cheapsharkLogo.svg'
+import diver from '../public/images/projectHeaders/diver.svg'
+import fish from '../public/images/projectHeaders/fish.svg'
 
 export default function CheapsharkProjectHeader() {
   const { state } = useContext(UserContext)
-  const { isPhone } = state.phone
+  const { isPhone, isPhonePortrait, isPhoneLandscape } = state.phone
 
   const useStyles = makeStyles({
     root: {
       width: '100%',
       height: '100%',
-      // background: '#272727',
+      background:
+        'linear-gradient(0deg, rgba(24,71,116,1) 0%, rgba(0,128,255,1) 100%)',
+
       padding: isPhone ? '5px' : '10px',
-    },
-    imageContainer: {
       position: 'relative',
-      height: '100%',
+    },
+    diverContainer: {
+      position: 'absolute',
+      right: isPhonePortrait ? 30 : isPhoneLandscape ? 50 : '5%',
+      top: isPhonePortrait ? 0 : isPhoneLandscape ? 0 : 10,
+      // background: 'green',
+    },
+    fishContainer: {
+      position: 'absolute',
+      left: isPhonePortrait ? 20 : isPhoneLandscape ? 50 : '5%',
+      top: isPhonePortrait ? 0 : isPhoneLandscape ? 0 : 20,
     },
   })
-
   const classes = useStyles()
+
+  const getLogoProps = () => {
+    const phone = { height: 60, width: 180 }
+    const normal = { height: 90, width: 270 }
+
+    return isPhone ? phone : normal
+  }
+
+  const logoProps = getLogoProps()
+
+  const getDiverProps = () => {
+    const portrait = { height: 50, width: 60 }
+    const landscape = { height: 50, width: 80 }
+    const normal = { height: 60, width: 150 }
+
+    return isPhonePortrait ? portrait : isPhoneLandscape ? landscape : normal
+  }
+
+  const diverProps = getDiverProps()
+
+  const getFishProps = () => {
+    const portrait = { height: 50, width: 60 }
+    const landscape = { height: 50, width: 80 }
+    const normal = { height: 100, width: 150 }
+
+    return isPhonePortrait ? portrait : isPhoneLandscape ? landscape : normal
+  }
+
+  const fishProps = getFishProps()
+
   return (
     <Grid
       container
       justifyContent="center"
       alignItems="center"
       className={classes.root}>
-      <Grid className={classes.imageContainer}></Grid>
-      CHEAPSHARK PROJECT HEADER
+      <Grid className={classes.imageContainer}>
+        <Image src={logo} layout="fixed" {...logoProps} />
+      </Grid>
+      <Grid className={classes.diverContainer}>
+        <Image src={diver} layout="fixed" {...diverProps} />
+      </Grid>
+      <Grid className={classes.fishContainer}>
+        <Image src={fish} layout="fixed" {...fishProps} />
+      </Grid>
     </Grid>
   )
 }
--- components/TekkenNetProjectHeader.js
+++ components/TekkenNetProjectHeader.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import Grid from '@mui/material/Grid'
 import Image from 'next/image'
 import { UserContext } from '../utils/UserContext'
-import character from '../public/images/projectHeaders/tekkenProjectHeader.svg'
+import image from '../public/images/projectHeaders/tekkenProjectHeader.svg'
 import logo from '../public/images/projectHeaders/tekkenNetLogo.svg'
 import { makeStyles } from '@mui/styles'
 
@@ -19,11 +19,12 @@ export default function TekkenNetProjectHeader() {
       position: 'relative',
       overflow: 'hidden',
     },
-    characterContainer: {
+    imageContainer: {
       height: '100%',
       position: 'absolute',
-      right: isPhonePortrait ? -200 : isPhoneLandscape ? -50 : 150,
-      top: isPhonePortrait ? -10 : isPhoneLandscape ? -10 : -10,
+      // right: isPhonePortrait ? -200 : isPhoneLandscape ? -50 : 0,
+      top: isPhonePortrait ? -10 : isPhoneLandscape ? -20 : -10,
+      // background: 'green',
     },
     logoContainer: {
       position: 'absolute',
@@ -40,20 +41,12 @@ export default function TekkenNetProjectHeader() {
       justifyContent="center"
       alignItems="center"
       className={classes.root}>
-      <Grid className={classes.characterContainer}>
+      <Grid className={classes.imageContainer}>
         <Image
-          src={character}
+          src={image}
           layout="fixed"
-          height={isPhone ? 400 : 600}
+          height={isPhonePortrait ? 300 : isPhoneLandscape ? 400 : 500}
           width={600}
-        />
-      </Grid>
-      <Grid className={classes.logoContainer}>
-        <Image
-          src={logo}
-          layout="fixed"
-          width={isPhone ? 200 : 350}
-          height={50}
         />
       </Grid>
     </Grid>
